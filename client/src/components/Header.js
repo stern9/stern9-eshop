c5375950b4823bbd0f85c185b808695127dc851f
astern3 <avram.stern@digitas.com>
2021-01-11 15:30:00 -0600

    Add to cart functionality implemented, styled the cart screen and changed the bootswatch template file

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="light" expand="lg" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand href="/">Stern9 eShop</Navbar.Brand>
